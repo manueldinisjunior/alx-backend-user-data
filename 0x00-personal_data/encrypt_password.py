74ad9241718892a01163aab5104250817623ea40
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-11-02 12:13:32 +0200

    encryption for passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python3
-"""A module for encrypting passwords.
-"""
+"""This module showcases password encryption with bcrypt"""
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Hashes a password using a random salt.
-    """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    """Salts and hashes a password"""
+    if password is not None and isinstance(password, str):
+        return bcrypt.hashpw(bytes(password, 'UTF-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Checks is a hashed password was formed from the given password.
-    """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    """Salts and hashes a password"""
+    if isinstance(hashed_password, bytes) and isinstance(password, str):
+        return bcrypt.checkpw(bytes(password, 'UTF-8'), hashed_password)
+    return False
 
+
+if __name__ == '__main__':
+    """Tests the code in this module"""
+    pwd = 'This is some text'
+    print(f'[{pwd}]: {hash_password(pwd)}')
+    print(is_valid(hash_password(pwd), pwd), '\n')
+
+    pwd = '1 l0v3 7h3 w1ld!'
+    print(f'[{pwd}]: {hash_password(pwd)}')
+    print(is_valid(hash_password(pwd), pwd), '\n')
+
+    pwd = ''
+    print(f'[{pwd}]: {hash_password(pwd)}')
+    print(is_valid(hash_password(pwd), pwd), '\n')
+
+    pwd = 2
+    print(f'[{pwd}]: {hash_password(pwd)}')
+    print(is_valid(hash_password(pwd), str(pwd)))
