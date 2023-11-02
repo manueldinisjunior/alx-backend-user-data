25330d3761d5e96dbe3545ebcbdfedb487b66fb8
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-11-02 11:41:07 +0200

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,87 +1,15 @@
 #!/usr/bin/env python3
-""" Regex-ing, Log formatter, Create logger, Connect to secure database,
-    Read and filter data """
-
-from typing import List
-import re
-import logging
-import os
-import mysql.connector
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
-""" containing the fields from user_data.csv that are considered PII. """
+""" Returns a salted, hashed password, byte in string """
+import bcrypt
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        """ constructor """
-        self.fields = fields
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        """ filter values in incoming log records """
-        return filter_datum(self.fields, self.REDACTION,
-                            super().format(record), self.SEPARATOR)
+def hash_password(password: str) -> bytes:
+    """ Returns byte string password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-def filter_datum(fields: List[str],
-                 redaction: str,
-                 message: str,
-                 separator: str) -> str:
-    """ returns the log message obfuscated """
-    for item in fields:
-        message = re.sub(fr'{item}=.+?{separator}',
-                         f'{item}={redaction}{separator}', message)
-    return message
-
-
-def get_logger() -> logging.Logger:
-    """ returns a logging.Logger object """
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-    handler = logging.StreamHandler()
-    handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    logger.addHandler(handler)
-    return logger
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """ returns a connector to the database """
-    return mysql.connector.connect(
-                    host=os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost'),
-                    database=os.environ.get('PERSONAL_DATA_DB_NAME', 'root'),
-                    user=os.environ.get('PERSONAL_DATA_DB_USERNAME'),
-                    password=os.environ.get('PERSONAL_DATA_DB_PASSWORD', ''))
-
-
-def main():
-    """ obtain a database connection using get_db and retrieve all rows in the
-        users table and display each row under a filtered format """
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-    result = cursor.fetchall()
-    for row in result:
-        message = f"name={row[0]}; " + \
-                  f"email={row[1]}; " + \
-                  f"phone={row[2]}; " + \
-                  f"ssn={row[3]}; " + \
-                  f"password={row[4]};"
-        print(message)
-        log_record = logging.LogRecord("my_logger", logging.INFO,
-                                       None, None, message, None, None)
-        formatter = RedactingFormatter(PII_FIELDS)
-        formatter.format(log_record)
-    cursor.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    main()
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Implement is_valid to validate provided password
+    matched hashed_password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
